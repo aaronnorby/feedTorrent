93cc2c3834f163ec3c2f7e197a773b6d251ece69
Aaron Norby <aaron.norby@gmail.com>
2015-12-02 11:39:50 -0800

    Add spin.js for a spinner while the ajax request is being made

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+var opts = {
+      lines: 11             // The number of lines to draw
+    , length: 7             // The length of each line
+    , width: 2              // The line thickness
+    , radius: 10            // The radius of the inner circle
+    , scale: 1.0            // Scales overall size of the spinner
+    , corners: 1            // Roundness (0..1)
+    , color: '#000'         // #rgb or #rrggbb
+    , opacity: 0.25          // Opacity of the lines
+    , rotate: 0             // Rotation offset
+    , direction: 1          // 1: clockwise, -1: counterclockwise
+    , speed: 1              // Rounds per second
+    , trail: 60            // Afterglow percentage
+    , fps: 20               // Frames per second when using setTimeout()
+    , zIndex: 2e9           // Use a high z-index by default
+    , className: 'spinner'  // CSS class to assign to the element
+    , top: '50%'            // center vertically
+    , left: '50%'           // center horizontally
+    , shadow: false         // Whether to render a shadow
+    , hwaccel: false        // Whether to use hardware acceleration (might be buggy)
+    , position: 'absolute'  // Element positioning
+};
+
+
+var entriesContainer = $('.entries')[0];
+var spinner = new Spinner(opts).spin(entriesContainer);
+
 $.getJSON("https://agile-thicket-5774.herokuapp.com/feed")
   .done(function(feed) {
-    console.log("feed: ", feed);
+
+    spinner.stop();
 
     var entrySource = $("#entry-template").html();
     var entryTemplate = Handlebars.compile(entrySource);
