1afbe56d98eee1a4abce1426ebaa03f78fa3eb0f
Aaron Norby <aaron.norby@gmail.com>
2015-12-02 11:28:22 -0800

    Add momentjs to reformat date to human-readable with handlebars helper

--- js/handlebarsHelpers.js
+++ js/handlebarsHelpers.js
@@ -6,3 +6,8 @@ Handlebars.registerHelper('getTitle', function(title) {
   return title.split(' | ')[0];
 });
 
+Handlebars.registerHelper('dateformat', function(feedDate) {
+  return moment(feedDate, 'ddd, DD MMM YYYY HH:mm:ss Z').format('MMMM Do, YYYY h:mm a');
+});
+
+
